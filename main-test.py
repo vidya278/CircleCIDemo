01a1fc018e55741ec7a9e444d5c8f2c34f480ef0
vidya gowda <vidyagowda@vidyas-MacBook-Air.local>
2022-04-08 22:19:08 +0530

    initia commit

---
+++ main-test.py
@@ -0,0 +1,9 @@
+
+from main import ADD
+
+def TestAdd():
+    assert ADD(2,3) == 5
+    print("Add works !!!")
+
+if __name__ == '__main__':
+    TestAdd()
---
+++ main.py
@@ -0,0 +1,8 @@
+def ADD(a,b):
+        return a+ b
+
+def SayHello():
+        print("hi bujji !!")
+
+if __name__ == '__main__':
+        SayHello()
