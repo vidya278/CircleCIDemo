d3a136c7843ad44898c06b9eb5ae38772aeffad9
vidya gowda <vidyagowda@vidyas-MacBook-Air.local>
2022-04-08 22:48:20 +0530

    Adds test to make sure 5 + 5 is equal to 10

--- main-test.py
+++ main-test.py
@@ -4,7 +4,7 @@ mkdir .circleci
 touch .circleci/config.yml
 def TestAdd():
     assert ADD(2,3) == 5
-    assert ADD(2,3) == 9
+    assert ADD(6,3) == 9
     print("Add works !!!")
 
 if __name__ == '__main__':
